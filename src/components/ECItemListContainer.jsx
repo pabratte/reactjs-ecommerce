cf2b4105fc97a91a12d33e03abcce38f10847104
Pablo Abratte <pabratte@gmail.com>
2021-08-07 15:03:44 -0300

    feat: ItemCount component

--- src/components/ECItemListContainer.jsx
+++ src/components/ECItemListContainer.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-
+import { Container, Row } from 'react-bootstrap';
+import  ECItemCount  from './ECItemCount';
 
 function ECItemListContainer(props) {
+  const onAdd = (name, count) => {
+    console.log(`${count} ${name} added to cart`)
+  }
   return (
     <Container>
-        <p class="mt-3">Welcome again, {props.usename}!</p>
+        
+        <p className="mt-3">Welcome again, {props.usename}!</p>
+        
+        <Row>
+          <div className="col-3">
+            <ECItemCount name="item #1" initial="1" stock="5" onAdd={onAdd}></ECItemCount>
+          </div>
+          <div className="col-3">
+            <ECItemCount name="item #2" initial="1" stock="10" onAdd={onAdd}></ECItemCount>
+          </div>
+          <div className="col-3">
+            <ECItemCount name="item #3" initial="1" stock="0" onAdd={onAdd}></ECItemCount>
+          </div>
+        </Row>
     </Container>
   );
 }
