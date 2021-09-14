55155d5b1f942ae84d0b11cc936f0f9348dc69dc
Pablo Abratte <pabratte@gmail.com>
2021-09-13 21:00:44 -0300

    feat: improve style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,30 +7,25 @@ import { Button, Container, Alert, Row, Col, Modal } from 'react-bootstrap';
 export default function Footer() {
     return (
         <div className="footer">
-            <Row>
-                <Col>
-                    asdasd
-                </Col>
-                    <Col>
-                    
-                    <a className="social-icon" href="https://wa.me/+5493564505913" target="_blank">
-                        <FontAwesomeIcon icon={faWhatsapp} />
-                    </a>
-                    <a className="social-icon" href="https://www.facebook.com/mimitossf" target="_blank">
-                        <FontAwesomeIcon icon={faFacebookF} />
-                    </a>
-                    <a className="social-icon" href="https://www.instagram.com/mimitossanfco" target="_blank">
-                        <FontAwesomeIcon icon={faInstagram} />
-                    </a>
+            <Container>
+                <Row>
+                    <Col className="col-left">
+                        Mimitos - Córdoba 195 - San Francisco
                     </Col>
-            </Row>
-                
-            
-            <div>
-                
-            
-            
-            </div>
+                    <Col className="col-right">
+                        
+                        <a className="social-icon" href="https://wa.me/+5493564505913" target="_blank">
+                            <FontAwesomeIcon icon={faWhatsapp} />
+                        </a>
+                        <a className="social-icon" href="https://www.facebook.com/mimitossf" target="_blank">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                        <a className="social-icon" href="https://www.instagram.com/mimitossanfco" target="_blank">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
